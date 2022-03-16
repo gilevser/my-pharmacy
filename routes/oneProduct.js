1d0ae27613cf6c2c7f8274190da149f733d9cdc7
Merdzhen <merdzhen.k@gmail.com>
2022-03-16 15:09:12 +0300

    one product - styles

--- routes/oneProduct.js
+++ routes/oneProduct.js
@@ -3,9 +3,9 @@ const { Product } = require('../db/models');
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  console.log(id);
+  // console.log(id);
   const product = await Product.findOne({ where: { id }, raw: true });
-  console.log(product);
+  // console.log(product);
   res.render('oneProduct', { product });
 });
 
