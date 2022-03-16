2167c2f93e83719832f5630c6b034216aabcf6bf
Merdzhen <merdzhen.k@gmail.com>
2022-03-17 00:32:47 +0300

    adding to cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const { Product } = require('../db/models');
 
 router.get('/', async (req, res) => {
-  res.render('cart');
+  const products = await Product.findAll();
+  // console.log('all prod ====>', products.length);
+  res.render('cart', { products });
 });
 
 module.exports = router;
