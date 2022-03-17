5764e9e37e9ccdd67f46637f5e9bd5def748aeab
Merdzhen <merdzhen.k@gmail.com>
2022-03-17 10:10:27 +0300

    one product link corrected

--- public/js/clientCart.js
+++ public/js/clientCart.js
@@ -5,6 +5,6 @@ console.log(products);
 for (const item of itemsArray) {
   const node = document.createElement('p');
   const textnode = document.createTextNode(`${item}`);
-  node.appendChild(textnode); // отлавливаю название добавленного товара
+  node.appendChild(textnode);
   document.getElementById('cart-items').appendChild(node);
 }
